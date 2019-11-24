d106f1fe57ac93b2239bc1f1d8c92f4ebb41528e
Rozaq Adi <reza.gerrard@gmail.com>
2019-11-24 11:49:52 +0700

    adding cardlist

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React, {Component} from 'react';
-import Card from './Card';
+import CardList from './CardList'
 import {robots} from './robots';
 
 class App extends Component {
   render(){
     return(
       <div>
-        <Card id={robots[0].id} name={robots[0].name} username={robots[0].username} email={robots[0].email}/>
-        <Card id={robots[1].id} name={robots[1].name} username={robots[1].username} email={robots[1].email}/>
-        <Card id={robots[2].id} name={robots[2].name} username={robots[2].username} email={robots[2].email}/>
-
+        <CardList robots={robots}/>
       </div>
     )
   }
--- src/Card.js
+++ src/Card.js
@@ -4,7 +4,7 @@ import 'tachyons';
 const Card = ({id, name, email}) => {
   return(
     <div className='tc bg-light-green dib br3 pa3 ma2 grow'>
-      <img alt='photo' src={`https://robohash.org/${id}`} height='200'/>
+      <img alt='photo' src={`https://robohash.org/${id}?size=200x200`} />
       <div>
         <h2>{name}</h2>
         <p>{email}</p>
