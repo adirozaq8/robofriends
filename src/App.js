85392e659add458ba637849141bd2a28725eddec
Rozaq Adi <reza.gerrard@gmail.com>
2019-11-27 07:27:55 +0700

    adding search box feature

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import React, {Component} from 'react';
-import CardList from './CardList'
+import CardList from './CardList';
+import SearchBox from './SearchBox';
 import {robots} from './robots';
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      robots: robots,
+      searchfield: ''
+    };
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value })
+  }
+
   render(){
+    const filteredRobot = this.state.robots.filter(robot => {
+      return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+    })
     return(
-      <div>
-        <CardList robots={robots}/>
+      <div className='tc'>
+        <h1>RoboFriends</h1>
+        <SearchBox searchChanges={this.onSearchChange}/>
+        <CardList robots={filteredRobot}/>
       </div>
     )
   }
