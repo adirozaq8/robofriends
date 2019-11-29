ddb76dc6d7a17a5b45f6c7d99b27716d73c6d42c
Rozaq Adi <reza.gerrard@gmail.com>
2019-11-29 08:36:42 +0700

    turning robots from hardcode to json api

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, {Component} from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import {robots} from './robots';
 import './App.css'
 
 class App extends Component {
   constructor(){
     super()
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: ''
     };
   }
@@ -17,17 +16,27 @@ class App extends Component {
     this.setState({ searchfield: event.target.value })
   }
 
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => {this.setState({robots: users})})
+  }
+
   render(){
     const filteredRobot = this.state.robots.filter(robot => {
       return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
     })
-    return(
-      <div className='tc'>
-        <h1 className='f1'>RoboFriends</h1>
-        <SearchBox searchChanges={this.onSearchChange}/>
-        <CardList robots={filteredRobot}/>
-      </div>
-    )
+    if(this.state.robots.length === 0){
+      return <h1>Loading</h1>
+    } else {
+      return(
+        <div className='tc'>
+          <h1 className='f1'>RoboFriends</h1>
+          <SearchBox searchChanges={this.onSearchChange}/>
+          <CardList robots={filteredRobot}/>
+        </div>
+      )
+    }
   }
 }
 
