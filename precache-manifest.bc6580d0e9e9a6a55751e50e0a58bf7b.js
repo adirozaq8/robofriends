2ad8aefeedeb1f939febb52bd65352c6ae197f84
Rozaq Adi <reza.gerrard@gmail.com>
2019-12-01 12:12:31 +0700

    Updates

---
+++ precache-manifest.bc6580d0e9e9a6a55751e50e0a58bf7b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cfe86955b02445887989f23a92ab4b8",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "98c095a8d5468baf10d4",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "b5b029e6902ffd74b621",
+    "url": "/robofriends/static/css/main.98df5464.chunk.css"
+  },
+  {
+    "revision": "98c095a8d5468baf10d4",
+    "url": "/robofriends/static/js/2.cad92894.chunk.js"
+  },
+  {
+    "revision": "b5b029e6902ffd74b621",
+    "url": "/robofriends/static/js/main.f0eabcc2.chunk.js"
+  },
+  {
+    "revision": "69ae533dd49bc2dbc7cd",
+    "url": "/robofriends/static/js/runtime-main.9f59819d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
